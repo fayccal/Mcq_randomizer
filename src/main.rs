baa2bf65a0264673b04233f04c6f406b05a78a10
Faycal <b.fayccal@gmail.com>
2020-12-14 14:04:58 +0100

    go to the line but still need fix

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use pdf_canvas::{BuiltinFont, FontSource, Pdf};
 use rand::prelude::*;
 
 fn create_qcm(num: i32, mut content: Vec<String>) {
-    for n in 1..num {
+    for n in 1..=num {
         let file_name = format!("../qcm_folder/qcm{}.pdf", n);
 
         let mut document = Pdf::create(&file_name).expect("Create pdf file");
@@ -14,16 +14,27 @@ fn create_qcm(num: i32, mut content: Vec<String>) {
         let mut height_to_right = 280.0;
         document
             .render_page(210.0, 297.0, |canvas| {
-                //canvas.set_line_join_style(pdf_canvas::graphicsstate::JoinStyle::Bevel);
-                // This closure defines the content of the page
                 while !content.is_empty() {
                     // Some text
-                    if let Some(hello) = content.pop() {
+                    if let Some(mut hello) = content.pop() {
+                        if hello.len() >= 70 {
+                            let reste = hello.split_off(70);
+                            canvas
+                                .left_text(10.0, height_to_right, font, 6.0, &hello)
+                                .expect("this is gonna work");
+                            height_to_right -= 10.0;
+                            
+                            canvas
+                                .left_text(10.0, height_to_right, font, 6.0, &reste)
+                                .expect("maybe gonna work");
+                        }
+                        else{
                         canvas
                             .left_text(10.0, height_to_right, font, 6.0, &hello)
                             .expect("please work");
-                            //1 si la taille d'une certain phrase est trop long on la coup a l'extrémité
-                            //2 page add automatique si trop de question
+                        //1 si la taille d'une certain phrase est trop long on la coup a l'extrémité(70)
+                        //2 page add automatique si trop de question
+                        }
                     }
                     height_to_right -= 10.0;
                 }
@@ -39,8 +50,8 @@ fn main() -> anyhow::Result<()> {
     let mut rng = rand::thread_rng();
     let mut vec_quest: Vec<String> = Vec::new();
     let how_much = 5;
-    
-       while let Some(mut question) = lines.next() {
+
+    while let Some(mut question) = lines.next() {
         while question == "" {
             question = lines.next().ok_or(anyhow!("Whut happend?"))?;
         }
@@ -48,7 +59,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     vec_quest.shuffle(&mut rng);
-    //println!("{:?}",vec_quest);
+    //  println!("{:?}",vec_quest);
 
     create_qcm(how_much, vec_quest);
 
